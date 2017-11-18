fd61d92c481fad4c8c6cbb3beeea8930c5ced257
Liz Adams <thelza12345@Elizabeths-MacBook-Pro-2.local>
2017-11-17 19:25:23 -0500

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,160 @@
-
-
 //variables
+
+//start screen
+	  $("#answerPage").hide();
+	  $(".jumbotron").hide();
+	  $("#Game").hide();
+      $("#start").click(function () {
+      $("#SplashScreen").hide();
+      $("#Game").show();
+      $(".jumbotron").show();
+      $("#done").click(function () {
+      $("#SplashScreen1").show();
+      $("#answerPage").show();
+});
+    });
 
 //timer: Countdown from 25 seconds to answer all questions
 window.onload = function() {
-  $("#start").on("click", stopwatch.start);
+    $("#start").on("click", timer.start);
 };
-//  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
-//prevents the clock from being sped up unnecessarily
+
 var clockRunning = false;
-// Our stopwatch object
-var stopwatch = {
-  time: 25,
-  lap: 1,
-  reset: function() {
-    stopwatch.time = 25;
-    stopwatch.lap = 1;
-    $("#display").text("00:25");
-    // DONE: Empty the "laps" div.
-    $("#laps").text("");
-  },
-  start: function() {
-    // DONE: Use setInterval to start the count here and set the clock to running.
-    if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count, 1000);
-        clockRunning = true;
+// timer object
+var timer = {
+    time: 30,
+
+    start: function() {
+        if (!clockRunning) {
+            intervalId = setInterval(timer.count, 1000);
+            clockRunning = true;
+        }
+    },
+    stop: function() {
+        clearInterval(intervalId);
+        clockRunning = false;
+       $("#answerPage").show();
+    },
+
+    count: function() {
+        
+        if (clockRunning) {
+        	timer.time--;
+        	var converted = timer.timeConverter(timer.time);
+        	console.log(converted);
+     
+       	$("#display").text(converted);
+	}
+    },
+    
+    timeConverter: function(t) {
+        var seconds = t;
+
+        if (seconds === 0) {
+            seconds = "00";
+            clockRunning = false;
+            clearInterval(intervalId);
+            stop();
+            $("#answerPage").show();
+        }
+
+        else if (seconds < 10) {
+            seconds = "0" + seconds;
+            $("#answerPage").hide();
+        }
+      
+		$("#done").on("click", timer.stop);
+		answerPage();
+		correctCounter();
+		incorrectCounter();
+
+        return "" + seconds;
     }
-  },
-  stop: function() {
-    // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-    clearInterval(intervalId);
-    clockRunning = false;
-  },
-  recordLap: function() {
-    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-    //       and save the result in a variable.
-    var converted = stopwatch.timeConverter(stopwatch.time);
-    // DONE: Add the current lap and time to the "laps" div.
-    $("#laps").append("<p>Lap " + stopwatch.lap + " : " + converted + "</p>");
-    // DONE: Increment lap by 1. Remember, we can't use "this" here.
-    stopwatch.lap++;
-  },
-  count: function() {
-    // DONE: increment time by 1, remember we cant use "this" here.
-    stopwatch.time--;
-    // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-    //       and save the result in a variable.
-    var converted = stopwatch.timeConverter(stopwatch.time);
-    console.log(converted);
-    // DONE: Use the variable we just created to show the converted time in the "display" div.
-    $("#display").text(converted);
-  },
-  timeConverter: function(t) {
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-    if (minutes === 0) {
-      minutes = "00";
-    }
-    else if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    return minutes + ":" + seconds;
-  }
 }
+	
+var userChoiceCorrect = ".correct";
+var userChoiceIncorrect = ".incorrect";
+var userDone = "#done";
+var correct = 0;
+var incorrect = 0;
 
-  
+
+var correctCounter = function() {
+      
+      document.querySelector("#correctDisp").innerHTML = correct;
+    };  
+
+var incorrectCounter = function() {
+      
+      document.querySelector("#incorrectDisp").innerHTML = incorrect;
+    };  
+//click events for radio buttons
+
+// 	function answerPage() {
+// 		if (userChoiceIncorrect == incorrect) {
+// 			$(incorrect += 1);
+// 			incorrectCounter();
+			
+
+// 		if (userChoiceCorrect == correct) {
+// 			$(correct += 1);
+// 			correctCounter();
+			
+// 		}
+
+// 		else if ("#done") {
+// 			answerPage();
+// 			stop();
+			
+
+// 		}
+// 	}
+// }
+	
 
 
+        
+         // $("#userChoice").removeAttr("disabled");
+function answerPage() {
+         if ($(".correct + userChoiceCorrect")) {
+              //correct question clicked
+              document.querySelector("#correctDisp").innerHTML = correct;
+              correctCounter();
+             correct += 1;
+         }
+         else {
+//             // wrong question clicked
+             ($(".incorrect + userChoiceIncorrect"));
+             document.querySelector("#incorrectDisp").innerHTML = incorrect;
+             incorrectCounter();
 
-//questions with options for answers
+             incorrect +=1;
+        }
+   }
+
+
+//DONE! need start page with start button
+
+//DONE! questions with options for answers
 //questions are true/false with 4 options for the correct answer
-//user can only choose one answer to each question
-//have to figure out how to add the inner HTML plus button choices
+//DONE! user can only choose one answer to each question
+//DONE!have to figure out how to add the inner HTML plus button choices
+
 
 
 //if user chooses correct answer it goes to the correct answers section on the 
-// end page that pops up after the timer is up
+	// end page that pops up after the timer is up
 
 //else the user chooses incorrectly that is added to the incorrect answers on the end page
 
-//how do we change the page once the timer is up and we need to see if the user won or lost the game?
+//DONE! need done button if user finishes before time is up -NOT DONE: to send them to the results page
 
-//way to reset the game?
+//how do we change the page once the timer is up and we need to see if the user won or lost the game?
+	
+	//create container in html innerHtml
+
+//way show correct incorrect page and show scores?
+
+
+
